9aabda40a306b015e09bd4cb517356e2182b5c7e
Bojie Jiang <jiangbojie@gmail.com>
2019-04-30 21:37:50 -0700

    field injection

--- app/src/main/java/us/bojie/dagger2example/MainActivity.java
+++ app/src/main/java/us/bojie/dagger2example/MainActivity.java
@@ -2,15 +2,20 @@ package us.bojie.dagger2example;
 
 import android.os.Bundle;
 
+import javax.inject.Inject;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Inject Car car;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         CarComponent carComponent = DaggerCarComponent.create();
-        carComponent.getCar().drive();
+        carComponent.inject(this);
+        car.drive();
     }
 }
