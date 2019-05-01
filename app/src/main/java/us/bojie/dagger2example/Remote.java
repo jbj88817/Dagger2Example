3aabda7b7da957e38944265d0e9899bc66bb5c8b
Bojie Jiang <jiangbojie@gmail.com>
2019-04-30 21:53:02 -0700

    method injection

---
+++ app/src/main/java/us/bojie/dagger2example/Remote.java
@@ -0,0 +1,18 @@
+package us.bojie.dagger2example;
+
+import android.util.Log;
+
+import javax.inject.Inject;
+
+public class Remote {
+    private static final String TAG = "Remote";
+
+    @Inject
+    public Remote() {
+
+    }
+
+    public void setListener(Car car) {
+        Log.d(TAG, "setListener: Remote connected");
+    }
+}
