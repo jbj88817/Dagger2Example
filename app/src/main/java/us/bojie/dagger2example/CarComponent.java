b0df28de7ccd13a0ec947cd36e13acca918f3713
Bojie Jiang <jiangbojie@gmail.com>
2019-04-30 19:28:33 -0700

    Setup and constructor injection

---
+++ app/src/main/java/us/bojie/dagger2example/CarComponent.java
@@ -0,0 +1,8 @@
+package us.bojie.dagger2example;
+
+import dagger.Component;
+
+@Component
+public interface CarComponent {
+    Car getCar();
+}
---
+++ app/src/main/java/us/bojie/dagger2example/Wheels.java
@@ -0,0 +1,10 @@
+package us.bojie.dagger2example;
+
+import javax.inject.Inject;
+
+public class Wheels {
+
+    @Inject
+    public Wheels() {
+    }
+}
